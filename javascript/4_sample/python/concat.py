70b5d59bbcc51479c48ee84ae895f8cbe0a8fbc8
yuginegi <yuginegi0980@gmail.com>
2023-01-05 12:14:06 +0900

    Add Sample-4 : Scroll Animation

---
+++ javascript/4_sample/python/concat.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+# python3.9 ene.py
+
+ll = [
+["pp1.png","pp2.png","pp3.png","pp2.png"],
+["pp4.png","pp5.png","pp6.png","pp5.png"]
+]
+
+# 透明度も引き継ぐならー１
+flag = -1
+im = []
+
+def hfunc(ll):
+  im = []
+  for ii in ll:
+    print(ii)
+    img1 = cv2.imread(ii,flag)
+    im.append(img1)
+  return cv2.hconcat(im)
+
+def vfunc(ll):
+  im = []
+  for aa in ll:
+    ii = hfunc(aa)
+    im.append(ii)
+  return cv2.vconcat(im)
+
+
+im = vfunc(ll)
+cv2.imwrite('../pict.png', im)
+
